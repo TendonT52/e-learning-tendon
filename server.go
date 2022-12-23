69ab64fb781b96108b0988322ab8eb7d4c84a291
Tendon.T52 <tendon.t52@gmail.com>
2022-12-23 20:38:41 +0700

    add server run

--- server.go
+++ server.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/TendonT52/e-learning-tendon/config"
 	"github.com/TendonT52/e-learning-tendon/handlers"
 )
 
 func main() {
+	fmt.Println("server is running")
 	config.LoadConfig()
 	config.SetupInstance()
 	handlers.SetupRouter()
